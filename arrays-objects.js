2c5b5274ce1123c1b0225b9638c8294df66105b4
kylelingat <kylejlingat@gmail.com>
2016-12-05 08:40:00 -1000

    Update arrays-objects.js

--- arrays-objects.js
+++ arrays-objects.js
@@ -5,3 +5,22 @@ Arrays and Objects
 Dec. 5, 2016
 
 */
+
+// variables 
+//array
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ['tan', 'zak', 'yolo', 'ohai', 'jef'];
+var colors = ['rainbow', 'black', 'ron burgandy', 'light black', 'highlighter'];
+var adj = ['smart', 'special case', 'yumy', 'tiny whiny', 'ferocious'];
+var slang = ['scrap', 'kobe beef', 'da kiine', 'liddat', 'pau'];
+
+// object
+
+var donut = {
+  toppings : ['sprinkles', 'frosting', 'ohare ayre'],
+  fillings : 'jelly',
+  shape : 'circle',
+  count : 12
+}
+
+
